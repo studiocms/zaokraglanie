e93bc237fdcd6d887675dfa180d386077e174b20
fotokolak <fotokolak@gmail.com>
2019-10-20 15:21:38 +0200

    Dodaj pliki projektów.

---
+++ zaokraglanie/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace zaokraglanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Zaokrąglenie liczby 2,25 w górę");
+            
+            var liczba = Math.Round(2.25, 1, MidpointRounding.AwayFromZero);
+            Console.WriteLine(liczba);
+
+
+        }
+    }
+}
